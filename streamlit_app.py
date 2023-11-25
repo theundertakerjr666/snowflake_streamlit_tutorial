b831fdec2a49afaac36bea0a2d899b46310278e9
contact <justinraj1984@protonmail.com>
2023-11-25 16:42:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.text('Thanks for adding ', if(add_my_fruit)!=null{add_my_fruit})
+if (add_my_fruit!=null) {
+ streamlit.text('Thanks for adding ', add_my_fruit)
+}
+
  
